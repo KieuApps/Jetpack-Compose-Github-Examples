ba13008e8fa15c70ee9225cad856bd1bc62e0d25
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-13 11:27:29 -0400

    Create BottomAppBar.kt

---
+++ BottomAppBar.kt
@@ -0,0 +1,22 @@
+data class Page(
+    val label: String,
+    val icon: ImageVector,
+)
+				
+var currentPage by rememberSaveable { mutableStateOf(1) }
+val pages = listOf(
+    Page("Contacts", Icons.Default.Person),
+    Page("Home", Icons.Default.Home),
+    Page("Notifications", Icons.Default.Notifications)
+)
+				
+BottomAppBar(Modifier.fillMaxWidth(0.8f)) {
+    pages.forEachIndexed { index, page ->
+        BottomNavigationItem(
+            selected = currentPage == index,
+            onClick = { currentPage = index },
+            icon = { Icon(page.icon, page.label) },
+            label = { Text(page.label) },
+        )
+    }
+}
