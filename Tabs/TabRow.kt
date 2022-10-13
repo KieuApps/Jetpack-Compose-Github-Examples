25354485a1fd0f5c119942383e04636510ff8b70
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-13 15:16:43 -0400

    Update TabRow.kt

--- Tabs/TabRow.kt
+++ Tabs/TabRow.kt
@@ -1,15 +1,10 @@
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.LeadingIconTab
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
@@ -18,8 +13,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
@@ -32,16 +25,27 @@ fun TabsExamplesComposable() {
 	
 	var currentPage by rememberSaveable { mutableStateOf(1) }
 	val pages = listOf(
-		Page("Acct", Icons.Default.AccountCircle),
 		Page("Home", Icons.Default.Home),
 		Page("Alert", Icons.Default.Notifications)
 	)
 	
-	
-	// TabRow - LeadingIconTab, Plain
+	// TabRow - Tab, Icon Only
 	TabRow(
-		selectedTabIndex = currentPage,
-		modifier = Modifier.fillMaxWidth(0.9f)
+		selectedTabIndex = currentPage
+	) {
+		pages.forEachIndexed { index, page ->
+			Tab(
+				selected = currentPage == index,
+				onClick = { currentPage = index },
+				icon = { Icon(page.icon, page.label) },
+				modifier = Modifier.padding(top = 8.dp)
+			)
+		}
+	}
+	
+	// TabRow - LeadingIconTab - Plain
+	TabRow(
+		selectedTabIndex = currentPage
 	) {
 		pages.forEachIndexed { index, page ->
 			LeadingIconTab(
@@ -54,101 +58,31 @@ fun TabsExamplesComposable() {
 		}
 	}
 	
-	// TabRow - LeadingIconTab, Custom Color
+	// TabRow - Tab, Text Only
 	TabRow(
-		selectedTabIndex = currentPage,
-		modifier = Modifier.fillMaxWidth(0.9f)
+		selectedTabIndex = currentPage
 	) {
 		pages.forEachIndexed { index, page ->
-			LeadingIconTab(
+			Tab(
 				selected = currentPage == index,
 				onClick = { currentPage = index },
 				text = { Text(page.label) },
-				icon = { Icon(page.icon, page.label) },
-				modifier = Modifier.padding(top = 8.dp),
-				selectedContentColor = Color(0xFFB554C5),
-				unselectedContentColor = Color(0xFFB554C5).copy(0.6f)
-			)
-		}
-	}
-	
-	// TabRow - LeadingIconTab, Custom Shape
-	TabRow(
-		selectedTabIndex = currentPage,
-		modifier = Modifier.fillMaxWidth(0.9f)
-	) {
-		pages.forEachIndexed { index, page ->
-			LeadingIconTab(
-				selected = currentPage == index,
-				onClick = { currentPage = index },
-				text = { Text(page.label) },
-				icon = { Icon(page.icon, page.label) },
-				modifier = Modifier
-					.background(MaterialTheme.colors.background)
-					.padding(top = 4.dp, start = 2.dp, end = 2.dp)
-					.clip(
-						RoundedCornerShape(
-							topStart = 8.dp,
-							topEnd = 8.dp
-						)
-					)
-					.background(MaterialTheme.colors.primary)
+				modifier = Modifier.padding(top = 8.dp)
 			)
 		}
 	}
 	
 	// TabRow - Tab, Plain
 	TabRow(
-		selectedTabIndex = currentPage,
-		modifier = Modifier.fillMaxWidth(0.9f)
+		selectedTabIndex = currentPage
 	) {
 		pages.forEachIndexed { index, page ->
 			Tab(
 				selected = currentPage == index,
 				onClick = { currentPage = index },
+				text = { Text(page.label) },
 				icon = { Icon(page.icon, page.label) },
 				modifier = Modifier.padding(top = 8.dp)
-			)
-		}
-	}
-	
-	// TabRow - Tab, Custom Color
-	TabRow(
-		selectedTabIndex = currentPage,
-		modifier = Modifier.fillMaxWidth(0.9f)
-	) {
-		pages.forEachIndexed { index, page ->
-			Tab(
-				selected = currentPage == index,
-				onClick = { currentPage = index },
-				icon = { Icon(page.icon, page.label) },
-				modifier = Modifier.padding(top = 8.dp),
-				selectedContentColor = Color(0xFFB554C5),
-				unselectedContentColor = Color(0xFFB554C5).copy(alpha = 0.6f)
-			)
-		}
-	}
-	
-	// TabRow - Tab, Custom Shape
-	TabRow(
-		selectedTabIndex = currentPage,
-		modifier = Modifier.fillMaxWidth(0.9f)
-	) {
-		pages.forEachIndexed { index, page ->
-			Tab(
-				selected = currentPage == index,
-				onClick = { currentPage = index },
-				icon = { Icon(page.icon, page.label) },
-				modifier = Modifier
-					.background(MaterialTheme.colors.background)
-					.padding(top = 4.dp, start = 2.dp, end = 2.dp)
-					.clip(
-						RoundedCornerShape(
-							topStart = 8.dp,
-							topEnd = 8.dp
-						)
-					)
-					.background(MaterialTheme.colors.primary)
 			)
 		}
 	}
