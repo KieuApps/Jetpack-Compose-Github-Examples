def6215caab53c0cb9f1ba5dfb007b2ecf97f21d
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-13 15:43:15 -0400

    Create TextStyle.kt

---
+++ Text/TextStyle.kt
@@ -0,0 +1,85 @@
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TextExamplesComposable() {
+	
+	// Text Style - Header 1
+	Text(
+		"h1 style",
+		style = MaterialTheme.typography.h1
+	)
+	
+	// Text Style - Header 2
+	Text(
+		"h2 style",
+		style = MaterialTheme.typography.h2
+	)
+	
+	// Text Style - Header 3
+	Text(
+		"h3 style",
+		style = MaterialTheme.typography.h3
+	)
+	
+	// Text Style - Header 4
+	Text(
+		"h4 style",
+		style = MaterialTheme.typography.h4
+	)
+	
+	// Text Style - Header 5
+	Text(
+		"h5 style",
+		style = MaterialTheme.typography.h5
+	)
+	
+	// Text Style - Header 6
+	Text(
+		"h6 style",
+		style = MaterialTheme.typography.h6
+	)
+	
+	// Text Style - Body 1
+	Text(
+		"body1 style",
+		style = MaterialTheme.typography.body1
+	)
+	
+	// Text Style - Body 2
+	Text(
+		"body2 style",
+		style = MaterialTheme.typography.body2
+	)
+	
+	// Text Style - Subtitle 1
+	Text(
+		"subtitle1 style",
+		style = MaterialTheme.typography.subtitle1
+	)
+	
+	// Text Style - Subtitle 2
+	Text(
+		"subtitle2 style",
+		style = MaterialTheme.typography.subtitle2
+	)
+	
+	// Text Style - Button
+	Text(
+		"button style",
+		style = MaterialTheme.typography.button
+	)
+	
+	// Text Style - Caption
+	Text(
+		"caption style",
+		style = MaterialTheme.typography.caption
+	)
+	
+	// Text Style - Overline
+	Text(
+		"overline style",
+		style = MaterialTheme.typography.overline
+	)
+}
