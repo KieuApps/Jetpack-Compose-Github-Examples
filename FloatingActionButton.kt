9bc5c00a80bded96ba29a2839a7b786dca570f83
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-13 13:34:08 -0400

    Create FloatingActionButton.kt

---
+++ FloatingActionButton.kt
@@ -0,0 +1,14 @@
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FloatingActionButtonExamplesComposable() {
+	FloatingActionButton(
+		onClick = { /* TODO */ }
+	) {
+		Icon(Icons.Default.Email, "Email")
+	}
+}
