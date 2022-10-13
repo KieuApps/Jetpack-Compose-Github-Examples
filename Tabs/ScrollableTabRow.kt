224ebaf646b7e22df69d10836581f16394ac6ea9
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-13 15:06:30 -0400

    Create ScrollableTabRow.kt

---
+++ Tabs/ScrollableTabRow.kt
@@ -0,0 +1,102 @@
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.LeadingIconTab
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TabsExamplesComposable() {
+	data class Page(
+		val label: String,
+		val icon: ImageVector,
+	)
+	
+	var currentPage by rememberSaveable { mutableStateOf(1) }
+	val pages = listOf(
+		Page("Acct", Icons.Default.AccountCircle),
+		Page("Home", Icons.Default.Home),
+		Page("Alert", Icons.Default.Notifications),
+		Page("Info", Icons.Default.Info),
+		Page("Share", Icons.Default.Share),
+		Page("Menu", Icons.Default.MoreVert),
+	)
+	
+	// ScrollableTabRow - Tab, Icon
+	ScrollableTabRow(
+		selectedTabIndex = currentPage,
+		modifier = Modifier.fillMaxWidth(0.9f)
+	) {
+		pages.forEachIndexed { index, page ->
+			Tab(
+				selected = currentPage == index,
+				onClick = { currentPage = index },
+				icon = { Icon(page.icon, page.label) },
+				modifier = Modifier.padding(top = 8.dp)
+			)
+		}
+	}
+	
+	// ScrollableTabRow - LeadingIconTab, Plain
+	ScrollableTabRow(
+		selectedTabIndex = currentPage,
+		modifier = Modifier.fillMaxWidth(0.9f)
+	) {
+		pages.forEachIndexed { index, page ->
+			LeadingIconTab(
+				selected = currentPage == index,
+				onClick = { currentPage = index },
+				text = { Text(page.label) },
+				icon = { Icon(page.icon, page.label) },
+				modifier = Modifier.padding(top = 8.dp)
+			)
+		}
+	}
+	
+	// ScrollableTabRow - Tab, Text
+	ScrollableTabRow(
+		selectedTabIndex = currentPage,
+		modifier = Modifier.fillMaxWidth(0.9f)
+	) {
+		pages.forEachIndexed { index, page ->
+			Tab(
+				selected = currentPage == index,
+				onClick = { currentPage = index },
+				text = { Text(page.label) },
+				modifier = Modifier.padding(top = 8.dp)
+			)
+		}
+	}
+	
+	// ScrollableTabRow - Tab, Plain
+	ScrollableTabRow(
+		selectedTabIndex = currentPage,
+		modifier = Modifier.fillMaxWidth(0.9f)
+	) {
+		pages.forEachIndexed { index, page ->
+			Tab(
+				selected = currentPage == index,
+				onClick = { currentPage = index },
+				text = { Text(page.label) },
+				icon = { Icon(page.icon, page.label) },
+				modifier = Modifier.padding(top = 8.dp)
+			)
+		}
+	}
+}
