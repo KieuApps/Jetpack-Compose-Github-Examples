9f72b703bb4a87b8813bf4e06d7dab4cb054f9f3
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-13 12:05:47 -0400

    Create Canvas.kt

---
+++ Canvas.kt
@@ -0,0 +1,20 @@
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CanvasExamplesComposable() {
+	
+	// Canvas - Plain
+	Canvas(
+		modifier = Modifier
+			.fillMaxWidth()
+			.height(100.dp)
+	) {
+		/* TODO */
+		
+	}
+}
