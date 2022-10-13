e230f5b7062d3d986d7c60a63c265d1a2d7a44bc
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-13 11:37:54 -0400

    Update BottomAppBar3.kt

--- BottomAppBar3.kt
+++ BottomAppBar3.kt
@@ -1,3 +1,22 @@
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.DEVELOPER_NAME.APP_NAME.util.*
+
 data class Page(
     val label: String,
     val icon: ImageVector,
