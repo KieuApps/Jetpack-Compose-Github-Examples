dd61be81ddf5f285c44e8091262b86a737347bff
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-13 13:31:59 -0400

    Create TextButton.kt

---
+++ TextButton.kt
@@ -0,0 +1,12 @@
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TextButtonExamplesComposable() {
+	TextButton(
+		onClick = { /* TODO */ }
+	) { 
+		Text("Button") 
+	}
+}
