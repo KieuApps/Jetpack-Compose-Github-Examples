2d6006adcda5158cdbb100ab590c6cfeb1722733
KieuApps <108589062+KieuApps@users.noreply.github.com>
2022-10-14 14:01:27 -0400

    Update Spacer.kt

--- More/Spacer.kt
+++ More/Spacer.kt
@@ -10,22 +10,34 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun SpacerExamplesComposable() {
+fun SpacerExamples() {
 	
 	// Spacer - Size
 	Spacer(Modifier.size(40.dp))
 	
+	
 	// Spacer - Width
 	Spacer(Modifier.width(160.dp))
+	
 	
 	// Spacer - Height
 	Spacer(Modifier.height(40.dp))
 	
-	// Spacer - Custom
+	
+	// Spacer - Custom, Wide
 	Spacer(
 		Modifier
 			.background(Color.Gray)
 			.height(10.dp)
 			.fillMaxWidth()
+	)
+	
+	
+	// Spacer - Custom, Tall
+	Spacer(
+		Modifier
+			.background(Color.Gray)
+			.height(120.dp)
+			.width(10.dp)
 	)
 }
